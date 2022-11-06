2fb765b19b963e296da5218d7acde93774836e9c
unknown <muh.it.eng.19841984@gmail.com>
2022-11-06 21:22:49 +0200

    init socket in client and server

--- server_app/src/index.ts
+++ server_app/src/index.ts
@@ -1,1 +1,23 @@
-console.log(" try to work ");
+import express from "express";
+import http from "http";
+import { Server } from "socket.io";
+import cors from "cors";
+const PORT = 8080;
+const app = express();
+app.use(cors());
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+  },
+});
+io.on("connection", (socket) => {
+  console.log("user is connected ");
+  socket.on("disconnect", () => {
+    console.log("user disonnected");
+  });
+});
+server.listen(PORT, () => {
+  console.log(`listen to server on PORT ${PORT}`);
+});
